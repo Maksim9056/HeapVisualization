41d3c870d7e32cb329e5cfc58cbf852ab631b0d5
Maksim <bobreczovm@inbox.ru>
2023-10-17 19:31:24 +0300

    Добавьте файлы проекта.

---
+++ HeapVisualization/HeapVisualization .cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeapVisualization
+{
+    public partial class HeapVisualization : Form
+    {
+        private List<int> heap;
+        private int numLevels;
+        private int maxElements;
+        private int  width;
+       private int height;
+        private float deltaX;
+        private float deltaY;
+      
+    public HeapVisualization(List<int> heap)
+    {
+        this.heap = heap;
+        numLevels = (int)Math.Ceiling(Math.Log(heap.Count + 1, 2));
+        maxElements = (int)Math.Pow(2, numLevels) - 1;
+        width = 800;
+        height = 600;
+        deltaX = width / (float)(numLevels + 1);
+        deltaY = height / (float)(heap.Count + 1);
+
+            InitializeForm();
+    }
+
+        private void InitializeForm()
+        {
+            ClientSize = new Size(width, height);
+            Paint += OnPaint;
+        }
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            //base.OnPaint(e);
+
+            Graphics graphics = e.Graphics;
+            Brush brush = Brushes.Red;
+            Font font = new Font("Helvetica", 8);
+            Pen pen = Pens.Black;
+
+            for (int i = 0; i < heap.Count; i++)
+            {
+                int level = (int)Math.Floor(Math.Log(i + 1, 2) + 1);
+                float x = deltaX * level;
+                float y = height - (deltaY * (i + 1));
+
+                //   graphics.FillEllipse(brush, x - 10, y - 10, 20, 20);
+                graphics.FillEllipse(brush, x - 10, y - 10, 20, 20);
+                graphics.DrawEllipse(pen, x - 10, y - 10, 20, 20);
+                x -= 5;
+                y -= 6;
+                graphics.DrawString(heap[i].ToString(), font, Brushes.Black, x, y);
+
+                if (i > 0)
+                {
+                    int parent = (int)Math.Floor((i - 1) / 2f);
+                    int parentLevel = (int)Math.Floor(Math.Log(parent + 1, 2) + 1);
+                    float parentX = deltaX * parentLevel;
+                    float parentY = height - (deltaY * (parent + 1));
+
+                    graphics.DrawLine(pen, parentX, parentY, x, y);
+                }
+            }
+        }
+
+        private void HeapVisualization_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+ 
+}
---
+++ HeapVisualization/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeapVisualization
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            List<int> heap = new List<int> { 4, 8, 10, 20, 5, 6, 13, 12, 25, 15, 14, 0, 26, 9, 3, 1, 19, 2, 24, 28, 17, 23, 27, 7, 11, 29, 21, 30, 22, 18, 16 };
+            Application.Run(new HeapVisualization(heap));
+        }
+    }
+}
